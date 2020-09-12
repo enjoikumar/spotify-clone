33373d5f15ff00a934fb88cbafc4cc0ffe3cbfb7
enjoikumar <kumar@secondchild.nyc>
2020-09-12 11:46:52 -0400

    updated reducer

--- spotify-clone-app/src/reducer.js
+++ spotify-clone-app/src/reducer.js
@@ -4,6 +4,8 @@ export const initialState = {
   playing: false,
   discover_weekly: null,
   item: null,
+  top_artists: null,
+  playing: false,
   // token: "BQBNJnDM0VfwU6kFEFZfUWc7JCUY_TPlZiwAOC4B8Sw-QCpYE_ZCJIZ3bVqDbIc7DsybTrr8nrwgD4o1vrlaO_YT73Fc16_EXeT74Z0j4uiVc_gj-R54O0TCK4PV_bTwdJx-l8ic6Ueu9F7ienXKIin-84uYw88"
   token: null
 }
@@ -24,17 +26,41 @@ const reducer = (state, action) => {
         token: action.token
       }
 
+    case "SET_PLAYING":
+      return {
+        ...state,
+        playing: action.playing,
+      };
+
+    case "SET_ITEM":
+      return {
+        ...state,
+        item: action.item,
+      };
+
     case 'SET_PLAYLISTS':
       return {
         ...state,
         playlists: action.playlists
       }
 
+    case "SET_TOP_ARTISTS":
+      return {
+        ...state,
+        top_artists: action.top_artists,
+      };
+
     case 'SET_DISCOVER_WEEKLY':
       return {
         ...state,
         discover_weekly: action.discover_weekly
       }
+      
+    case "SET_SPOTIFY":
+      return {
+        ...state,
+        spotify: action.spotify,
+      };
 
     default:
       return state;
